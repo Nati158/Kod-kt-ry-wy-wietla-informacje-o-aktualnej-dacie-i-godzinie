1fce2691d7e568984eb55a43d1551c1757bac60e
Nati158 <141584277+Nati158@users.noreply.github.com>
2024-03-03 09:24:21 +0100

    Add files via upload

---
+++ main39.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+def display_current_datetime():
+    current_datetime = datetime.now()
+    print("Current date and time:", current_datetime)
+
+if __name__ == "__main__":
+    display_current_datetime()
